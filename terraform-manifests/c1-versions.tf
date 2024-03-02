ce114edf2c9a14381a22997f2fa62f3445896c25
Vinod S <vinodoct2019@gmail.com>
2024-03-02 20:04:11 +0100

    updated RG for backend storage

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
   }
    backend "azurerm" {
-    resource_group_name   = "terraform-storage-rg01"
+    resource_group_name   = "AZ-300"
     storage_account_name  = "terraformstate112"
     container_name        = "tfstatefiles"
     key                   = "terraform.tfstate"
